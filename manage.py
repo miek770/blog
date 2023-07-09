723c8c7823943d78c0322458f3c19595cfbdd401
Michel Lavoie <lavoie.michel@gmail.com>
2023-07-08 20:33:37 -0400

    Fixed to work with Markdown only. Images don't show up without Nginx to redirect media files though

--- manage.py
+++ manage.py
@@ -4,6 +4,18 @@ import shutil
 from pathlib import Path
 import subprocess as sub
 import platform
+import fileinput
+
+
+def retarget_media_files(path: Path):
+    # Open the file in place for editing
+    with fileinput.FileInput(path, inplace=True) as file:
+        # Iterate over each line in the file
+        for line in file:
+            # Replace the target string with the desired replacement
+            updated_line = line.replace("![png](2023-07-xx_files/", "![png](../media/")
+            # Print the modified line to the file
+            print(updated_line, end='')
 
 
 def get_first_400_characters(file: Path) -> str:
@@ -40,18 +52,10 @@ def main(file_path: str):
             elif platform.system() == 'Linux':
                 python = "python3"
 
-            # Generate the HTML article
-            sub.run([
-                python,
-                "-m",
-                "nbconvert",
-                "--to",
-                "html",
-                "--output-dir=web/articles",
-                f"raws/{file_name}.ipynb",
-                ])
+            # Retarget file to the temporary (Markdown) one
+            file = Path(f"raws/tmp/{file_name}.md")
 
-            # Prepare the temporary markdown file for the brief
+            # Prepare the temporary markdown file
             sub.run([
                 python,
                 "-m",
@@ -62,14 +66,24 @@ def main(file_path: str):
                 f"raws/{file_name}.ipynb",
                 ])
 
+            # Copy the article as is
+            shutil.copy(file, f"web/articles/{file_name}.md")
+
             # Write the brief
-            file = Path(f"raws/tmp/{file_name}.md")
             with open(f"web/briefs/{file_name}.md", "w", encoding='utf-8') as f:
                 f.write(get_first_400_characters(file))
                 f.write("...")
 
+            # Copy all figures to the media directory
+            png_files = Path(f"raws/tmp/{file_name}_files").glob("*.png")
+            for f in png_files:
+                shutil.copy2(f, Path("web/media"))
+
             # Delete the temporary files
             shutil.rmtree("raws/tmp")
+
+            # Update the media links
+            retarget_media_files(Path(f"web/articles/{file_name}.md"))
 
 
 if __name__ == "__main__":
