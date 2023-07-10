de43d2c0331b0ef9013b77d71a13e1400617f008
Michel Lavoie <lavoie.michel@gmail.com>
2023-07-09 21:34:27 -0400

    Trying to optimize the image size and resolution; not easy

--- tex2png.sh
+++ tex2png.sh
@@ -14,7 +14,13 @@ pdflatex -output-directory tmp "\def\formula{$1}\input{formula.tex}"
 
 # Create the PNG
 # https://stackoverflow.com/questions/52998331/imagemagick-security-policy-pdf-blocking-conversion
-convert -density 300 "tmp/formula.pdf" -quality 90 "tmp/$2.png"
+# convert -density 300 "tmp/formula.pdf" -quality 90 "tmp/$2.png"
+# convert "tmp/formula.pdf" -resize 200% -quality 90 "tmp/$2.png"
+# convert "tmp/formula.pdf" -scale 200% -quality 90 "tmp/$2.png"
+#convert "tmp/formula.pdf" -colorspace RGB +sigmoidal-contrast 11.6933 \
+#  -define filter:filter=Sinc -define filter:window=Jinc -define filter:lobes=3 \
+#  -resize 150% -sigmoidal-contrast 11.6933 -colorspace sRGB -quality 100 "tmp/$2.png"
+convert "tmp/formula.pdf" -scale 150% -quality 100 "tmp/$2.png"
 
 # Cleanup
 rm tmp/formula.*
