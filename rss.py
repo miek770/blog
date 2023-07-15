ecd4a65208d1f05f5b112ae2f4e89f2c9bb794cb
Michel Lavoie <lavoie.michel@gmail.com>
2023-07-15 18:48:16 -0400

    Updated publish; added content to RSS feed with link to article

--- rss.py
+++ rss.py
@@ -44,7 +44,7 @@ def main():
         fe.link(href=id)
         fe.description(description)
         fe.author({"name": config["Site"]["author"], "email": config["Site"]["email"]})
-        fe.content(f"See full article here: {id}")
+        fe.content(f'See full article at <a href="{id}">{id}</a>')
 
         date = datetime.strptime(pub_date, "%Y-%m-%d")
         timezone = pytz.timezone(config["Site"]["timezone"])
