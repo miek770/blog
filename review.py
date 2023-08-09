88dd51289ca70b9000050e6462b17be29cef47cd
Michel Lavoie <lavoie.michel@gmail.com>
2023-08-09 09:18:07 -0400

    Added a model argument to allow using other models, gpt-4 for example (if a token size of 8k is sufficient)

--- review.py
+++ review.py
@@ -5,12 +5,12 @@ from pathlib import Path
 
 @click.command()
 @click.argument("file_path", type=str)
-def main(file_path: str):
+@click.argument("model", type=str, default="gpt-3.5-turbo-16k")
+def main(file_path: str, model: str):
     file = Path(file_path)
     if file.is_file():
         openai.api_key = Path("openai_api_key.txt").read_text()
         system_role = "You are a proofreader for my personal blog posts."
-        model = "gpt-3.5-turbo-16k"
         request = f"""Look for grammatical errors in the following blog post.
 Clearly highlight the errors in your response. Don't return the whole blog
 post; limit your response to the errors and their context:\n\n
